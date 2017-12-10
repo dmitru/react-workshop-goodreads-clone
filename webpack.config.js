395c62ee4caaa7ba150d26d160e694634b7196bc
Dmitry Borody <dmitriy.borodiy@gmail.com>
2017-12-10 23:50:12 +0400

    Add webpack-dev-server

--- webpack.config.js
+++ webpack.config.js
@@ -2,14 +2,18 @@ const path = require('path');
 
 module.exports = {
   context: __dirname,
-  entry: './src/index.js',
+  entry: ['./src/index.js', 'webpack-dev-server/client?http://localhost:8080'],
   devtool: 'cheap-eval-source-map',
   output: {
     path: path.join(__dirname, 'public/dist'),
     filename: 'bundle.js',
+    publicPath: '/dist/',
   },
   resolve: {
     extensions: ['.js', '.jsx', '.json'],
+  },
+  devServer: {
+    contentBase: path.join(__dirname, 'public'),
   },
   stats: {
     colors: true,
